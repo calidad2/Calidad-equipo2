6eed16adbc21a64c9adccb1b1970b4d2e02fcbdc
Miri <61881038+miri-fa@users.noreply.github.com>
2021-03-28 12:12:36 +0200

    Resolve bug with random

--- app/src/main/java/com/aplicacionps/PantallaFinal.java
+++ app/src/main/java/com/aplicacionps/PantallaFinal.java
@@ -46,8 +46,7 @@ public class PantallaFinal extends AppCompatActivity {
         //Se obtiene un numero aleatorio que dependiendo del porcentaje final hará que aparezca un
         //Mensaje diferente cada vez
 
-        Random r = new Random();
-        int numAleatorio = r.nextInt(100);
+        int numAleatorio = (int) (Math.random()*100);
 
         //Si el porcentaje final obtenido es 0 no se puede contagiar
         if (porcentajeActual == 0) {
