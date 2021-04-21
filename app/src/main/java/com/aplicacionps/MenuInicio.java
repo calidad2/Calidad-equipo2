6717ba7f6950f464414b44b6068f28bf605735a4
Miri <61881038+miri-fa@users.noreply.github.com>
2021-04-21 10:08:08 +0200

    Gherkins

--- app/src/main/java/com/aplicacionps/MenuInicio.java
+++ app/src/main/java/com/aplicacionps/MenuInicio.java
@@ -9,6 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.content.Intent;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
 import androidx.navigation.Navigation;
 
 import android.widget.ImageButton;
